62e37f554df2d28d071212cba1c310e93c48d9ef
Shashwat Singh <shashwat@kalpvaig.com>
2016-10-10 07:52:57 +0530

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core'
+
+@Component({
+    selector: 'app',
+    template: '<h1>My App</h1>',
+    styles: ['body{background: #ccc;}']
+})
+
+export class AppComponent{}
